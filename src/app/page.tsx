148059741c68dfdfa2890d72edb53df894cc6a71
Julian Curalli <julian.curalli27@gmail.com>
2024-09-23 19:28:51 -0400

    updated condition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default async function Landing() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       This is a sample home page
-      {!user!.isSignupComplete && (
-        <CompleteProfileModal clerkID={user!.clerkId} />
+      {user && !user.isSignupComplete && (
+        <CompleteProfileModal clerkID={user.clerkId} />
       )}
     </div>
   );
