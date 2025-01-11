729f8f8f46554b250c7a1fb8ea74ec35d17210fd
jho1234 <48388373+jho1234@users.noreply.github.com>
2025-01-11 15:45:33 -0500

    Finished match and team selection page!

--- src/app/manav/match-selection/match-selection-form.tsx
+++ src/app/manav/match-selection/match-selection-form.tsx
@@ -2,9 +2,18 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ChevronDown, ChevronDownIcon } from "lucide-react";
+import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "~/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "~/components/ui/command";
 import {
   Form,
   FormControl,
@@ -20,7 +29,13 @@ import {
   PopoverTrigger,
 } from "~/components/ui/popover";
 
-export default function MatchSelectionForm() {
+export default function MatchSelectionForm({
+  setTeamSelectedEnabled: setTeamSelectedEnabled,
+}: {
+  setTeamSelectedEnabled: (value: boolean) => void;
+}) {
+  const [isMatchSelectionOpen, setIsMatchSelectionOpen] = useState(false);
+
   const matchSelectionSchema = z.object({
     matchNumber: z
       .string({ required_error: "Match number is required" })
@@ -58,38 +73,78 @@ export default function MatchSelectionForm() {
           name="matchNumber"
           control={matchSelectionForm.control}
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Match number</FormLabel>
+            <FormItem className="flex flex-col">
               <FormControl>
-                <Input {...field} placeholder="Match number here..." />
+                <Popover
+                  open={isMatchSelectionOpen}
+                  onOpenChange={setIsMatchSelectionOpen}
+                >
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className="flex flex-row justify-between h-16 text-xl"
+                    >
+                      {field.value ? `Match ${field.value}` : "Select Match"}
+                      <ChevronDownIcon className="!size-6" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent sideOffset={-64}>
+                    <Command className="w-94">
+                      <CommandInput
+                        placeholder="Search Match"
+                        className="h-16 text-xl"
+                      />
+                      <CommandList className="mb-4">
+                        <CommandEmpty>Nothing found</CommandEmpty>
+                        <CommandGroup>
+                          {/* <CommandItem
+                            onSelect={(value) => {
+                              field.onChange(value);
+                              setIsMatchSelectionOpen(false);
+                            }}
+                          >
+                            Match 1
+                          </CommandItem> */}
+                          {Array.of(
+                            [
+                              1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                              15,
+                            ].map((value, index) => (
+                              <CommandItem
+                                className="h-16 text-lg"
+                                key={`match-dropdown-item-${index + 1}`}
+                                onSelect={(value) => {
+                                  const cleanVal = value.split(" ");
+
+                                  field.onChange(cleanVal[1]);
+                                  setIsMatchSelectionOpen(false);
+                                  setTeamSelectedEnabled(true);
+                                }}
+                              >
+                                {`Match ${value}`}
+                              </CommandItem>
+                            ))
+                          )}
+                          {/* <CommandItem
+                            onSelect={(value) => {
+                              const cleanVal = value.split(" ");
+
+                              field.onChange(cleanVal[1]);
+                              setIsMatchSelectionOpen(false);
+                            }}
+                          >
+                            Match 3
+                          </CommandItem> */}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-
-        <FormField
-          name="matchNumber"
-          control={matchSelectionForm.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Match selection</FormLabel>
-              <FormControl>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button variant={"outline"}>
-                      Select Match <ChevronDownIcon />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent></PopoverContent>
-                </Popover>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button className="!bg-green-500 flex items-center justify-center text-black font-bold">
-          Confirm
-        </Button>
       </form>
     </Form>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,12 +20,18 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+        blueTeam: " dark:bg-blue-600 p-3 text-center text-white",
+        redTeam: "dark:bg-red-600 p-3 text-center text-white",
+        proceed:
+          "bg-green-500 flex items-center justify-center text-black font-bold  text-xl",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        team: "h-16 w-32",
+        proceed: "h-20 px-4 py-2",
       },
     },
     defaultVariants: {
