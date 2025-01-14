1a1cafff7ab38a215aff097c2507665e35908133
Thejas <chandruthejas07@gmail.com>
2025-01-13 21:52:43 -0500

    changing name of the page to the one we'll be working on friday

--- src/app/thejas/page.tsx
+++ src/app/thejas/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Input } from"~/components/ui/input";
 
-export default function autonomousPage() {
+export default function matchSetup() {
   return(
     <p>yeah</p>
   );
