fe672befa668baeab532fdfaa7b351de31b7135f
Your Name <you@example.com>
2025-01-24 18:16:13 -0500

    ball scoring screen not finished

--- src/app/scout/[eventCode]/page.tsx
+++ src/app/scout/[eventCode]/page.tsx
@@ -275,7 +275,6 @@ const ScoutPage = () => {
         }}
       >
         <ScoutingInfoHeader />
-        <BallScoringScreen />
         {screens[currentScreenIndex].component}
         <ScoutActionButton
           actionName={ACTION_NAMES.INTAKE}
