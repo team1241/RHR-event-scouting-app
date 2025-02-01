1f1a75177d4987876d1ffcf3a3d044158d1ff7d6
Julian Curalli <julian.curalli27@gmail.com>
2025-01-31 22:48:58 -0500

    changes needed for deployments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)", "/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/admin(.*)",
+  "/dashboard(.*)",
+  "/scout(.*)",
+  "/pits(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
