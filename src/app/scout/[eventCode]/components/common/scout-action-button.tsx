a0b756bd6b2f82b5ec65ac2ace8feea0847a4c4d
jho1234 <48388373+jho1234@users.noreply.github.com>
2025-02-01 17:05:01 -0500

    Endgame mostly done  minus the ui/styles

--- src/app/scout/[eventCode]/components/common/scout-action-button.tsx
+++ src/app/scout/[eventCode]/components/common/scout-action-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { ReactNode, useContext } from "react";
 import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 import { ScoutDataContext } from "../../context";
@@ -24,6 +24,7 @@ const ScoutActionButton = ({
   isAuto?: boolean;
   className?: string;
   label: string;
+  children: ReactNode;
   onClick?: () => void;
   disabled?: boolean;
 }) => {
@@ -39,7 +40,7 @@ const ScoutActionButton = ({
         scoutId: scoutDataContext.scouterDetails.id.toString(),
         matchNumber: scoutDataContext.matchNumber,
         teamNumber: scoutDataContext.teamToScout!,
-        eventCode: scoutDataContext.matchNumber, 
+        eventCode: scoutDataContext.matchNumber,
         hasUndo: scoutDataContext.undoOccurred,
         wasDefended: scoutDataContext.wasDefended,
         actionName,
