31644d5a304e960916a3a7ace4899b1ab35fcc25
jho1234 <48388373+jho1234@users.noreply.github.com>
2025-02-10 11:52:54 -0500

    Fixed endgame bug (#25)
    * Endgame mostly done  minus the ui/styles
    
    * Finished endgame screen
    
    * updated prisma version
    
    * prisma v 19
    
    * IT DOESNT WORK AHHHHHHHHHHHHHHHHHHHHHHHHHHH
    
    * ╰(*°▽°*)╯╰(*°▽°*)╯FIXED IT!!!!! ╰(*°▽°*)╯╰(*°▽°*)╯

--- src/app/scout/[eventCode]/components/endgame-screen.tsx
+++ src/app/scout/[eventCode]/components/endgame-screen.tsx
@@ -28,6 +28,17 @@ export default function EndgameScreen() {
   const [hangPositionSelected, setHangPositionSelected] = useState<string>("");
   const [currentAction, setCurrentAction] = useState<string>("");
   const [isMatchSelectionOpen, setIsMatchSelectionOpen] = useState(false);
+  const [actionDone, setActionDone] = useState(false);
+  const [positionSelected, setPositionSelected] = useState(false);
+  function checkActionButtonDisabled() {
+    let disability = false;
+    if (!positionSelected || actionDone) {
+      disability = true;
+    }
+
+    return disability;
+  }
+
   return (
     <>
       <div className="flex flex-row">
@@ -37,8 +48,12 @@ export default function EndgameScreen() {
           </div>
           <UndoActionButton
             onClick={() => {
-              setHangPositionSelected("");
-              setCurrentAction("");
+              if (hangPositionSelected !== "") {
+                setHangPositionSelected("");
+                setActionDone(false);
+                setCurrentAction("");
+                setPositionSelected(false);
+              }
             }}
             className="text-xl bg-red-600 h-16 w-40"
           />
@@ -60,9 +75,10 @@ export default function EndgameScreen() {
               location={hangPositionSelected}
               className=" mb-1 h-16 w-40 font-bold text-xl bg-pink-600"
               onClick={() => {
+                setActionDone(true);
                 setCurrentAction("Successfully Climbed Shallow");
               }}
-              disabled={hangPositionSelected === ""}
+              disabled={checkActionButtonDisabled()}
               label="Shallow Hang"
             />
             <ScoutActionButton
@@ -71,9 +87,10 @@ export default function EndgameScreen() {
               location={hangPositionSelected}
               className=" mb-1 h-16 w-40 font-bold text-xl bg-pink-600"
               onClick={() => {
+                setActionDone(true);
                 setCurrentAction("Successfully Climbed Deep");
               }}
-              disabled={hangPositionSelected === ""}
+              disabled={checkActionButtonDisabled()}
               label="Deep Hang"
             />
 
@@ -83,9 +100,10 @@ export default function EndgameScreen() {
               location={hangPositionSelected}
               className=" mb-1 h-16 w-40 font-bold text-xl bg-pink-600"
               onClick={() => {
+                setActionDone(true);
                 setCurrentAction("Successfully Parked");
               }}
-              disabled={hangPositionSelected === ""}
+              disabled={checkActionButtonDisabled()}
               label="Park"
             />
 
@@ -93,10 +111,10 @@ export default function EndgameScreen() {
               open={isMatchSelectionOpen}
               onOpenChange={setIsMatchSelectionOpen}
             >
-              <PopoverTrigger disabled={hangPositionSelected === ""}>
+              <PopoverTrigger disabled={checkActionButtonDisabled()}>
                 <Button
                   className=" mb-1 h-16 w-40 font-bold text-xl !text-white !bg-pink-600"
-                  disabled={hangPositionSelected === ""}
+                  disabled={checkActionButtonDisabled()}
                 >
                   {" "}
                   Failed Attempt
@@ -110,9 +128,11 @@ export default function EndgameScreen() {
                   location={hangPositionSelected}
                   className="mb-1 h-16 w-40 font-bold text-xl !bg-pink-600"
                   onClick={() => {
+                    setActionDone(true);
                     setCurrentAction("Shallow Climb Failed ");
                     setIsMatchSelectionOpen(false);
                   }}
+                  disabled={checkActionButtonDisabled()}
                   label="Shallow Hang"
                 />
 
@@ -122,9 +142,11 @@ export default function EndgameScreen() {
                   location={hangPositionSelected}
                   className=" mb-1 h-16 w-40 font-bold text-xl !bg-pink-600"
                   onClick={() => {
+                    setActionDone(true);
                     setCurrentAction("Deep Climb Failed");
                     setIsMatchSelectionOpen(false);
                   }}
+                  disabled={checkActionButtonDisabled()}
                   label="Deep Hang"
                 />
               </PopoverContent>
@@ -134,8 +156,9 @@ export default function EndgameScreen() {
               gamePiece={GAME_PIECES.NOGAMEPIECE}
               location={hangPositionSelected}
               className=" mb-1 h-16 w-40 font-bold text-xl bg-pink-600"
-              disabled={hangPositionSelected === ""}
+              disabled={checkActionButtonDisabled()}
               onClick={() => {
+                setActionDone(true);
                 setCurrentAction("Endgame action skipped");
               }}
               label="Not Attempted"
@@ -155,8 +178,10 @@ export default function EndgameScreen() {
                   "dark:ring-2 ring-yellow-400  ring-offset-4"
               )}
               onClick={() => {
+                setPositionSelected(true);
                 setHangPositionSelected(LOCATIONS.BARGE.OUTER);
               }}
+              disabled={hangPositionSelected !== ""}
             >
               Far Cage
             </Button>
@@ -168,7 +193,9 @@ export default function EndgameScreen() {
               )}
               onClick={() => {
                 setHangPositionSelected(LOCATIONS.BARGE.MIDDLE);
+                setPositionSelected(true);
               }}
+              disabled={hangPositionSelected !== ""}
             >
               Middle Cage
             </Button>
@@ -179,8 +206,10 @@ export default function EndgameScreen() {
                   "dark:ring-2 ring-yellow-400  ring-offset-4"
               )}
               onClick={() => {
+                setPositionSelected(true);
                 setHangPositionSelected(LOCATIONS.BARGE.INNER);
               }}
+              disabled={hangPositionSelected !== ""}
             >
               Near Cage
             </Button>
