3f5b444980c8466784905984b9e9a10b80e78592
Julian Curalli <julian.curalli27@gmail.com>
2025-02-20 17:37:30 -0500

    added is defending string

--- src/app/scout/[eventCode]/components/common/match-scouting-layout.tsx
+++ src/app/scout/[eventCode]/components/common/match-scouting-layout.tsx
@@ -64,7 +64,7 @@ export const MatchScoutingLayout = ({
   return (
     <div>
       <p className="text-3xl font-bold my-1">
-        {getHasCoralOrAlgae(hasCoral, hasAlgae)}
+        {getHasCoralOrAlgae(hasCoral, hasAlgae, context.isDefending)}
       </p>
       <div>
         <FieldImage imageSize="100%" fieldSize="half">
