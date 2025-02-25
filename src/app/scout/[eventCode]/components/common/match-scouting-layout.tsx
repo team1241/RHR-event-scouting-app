d97f62e64aae850f510be59ddbef0a6ab2ac62ad
Thejas <chandruthejas07@gmail.com>
2025-02-25 17:57:27 -0500

    fixed lint

--- src/app/scout/[eventCode]/components/common/match-scouting-layout.tsx
+++ src/app/scout/[eventCode]/components/common/match-scouting-layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { cn } from "~/lib/utils";
 import {
   ACTION_NAMES,
